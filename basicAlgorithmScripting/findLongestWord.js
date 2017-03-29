176deef819a31dcf85993a62d8d5d7fb2235a66b
Pedro Garboza <p.garboza@gmail.com>
2017-03-28 21:37:32 -0400

    Added findLongestWord.js

--- basicAlgorithmScripting/findLongestWord.js
+++ basicAlgorithmScripting/findLongestWord.js
@@ -1,11 +1,17 @@
 function findLongestWord(str) {
 	  var numArr = [];
+	  //slipt str into array
 	  var wordArr = str.split(" ");
+	  //traverse array until the the limit, .length
 	  for(var i = 0; i < wordArr.length; i++){
+		      //copy num value of each word into new array
 		      numArr[i] = wordArr[i].length;
 		    }
+	  //sorts in ascending order including multi digit numbers
 	  numArr.sort(function(a, b){return a-b;});
+	  //reverses array order
 	  numArr.reverse();
+	  //outputs first and larger value in array
 	  return numArr[0];
 	}
 
