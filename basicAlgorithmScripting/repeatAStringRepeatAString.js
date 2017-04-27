4caba95996812edcae421da91ac1d7d9a469e861
Pedro Garboza <p.garboza@gmail.com>
2017-04-26 22:59:28 -0400

    Create repeatAStringRepeatAString.js

---
+++ basicAlgorithmScripting/repeatAStringRepeatAString.js
@@ -0,0 +1,29 @@
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  
+  //initialized empty string
+  var string = "";
+  
+  //if num is positive
+  if(num > 0){
+  
+      //traverse the array num times
+      for(var i = 0; i < num; i++){
+        
+        //concatinate add string value into string
+        string += str.concat();
+        
+      }
+      
+    //return string
+    return string;
+  }else{
+  
+    //else return nothing
+    return "";
+  }
+  
+
+}
+
+repeatStringNumTimes("abc", 3);
