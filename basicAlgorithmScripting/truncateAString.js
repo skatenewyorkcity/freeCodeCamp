49b64dc83c1674c6eb51718d9d9cb6cfa9fdb784
Pedro Garboza <p.garboza@gmail.com>
2017-05-03 23:38:50 -0400

    Update truncateAString.js

--- basicAlgorithmScripting/truncateAString.js
+++ basicAlgorithmScripting/truncateAString.js
@@ -1,1 +1,14 @@
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  
+  if(str.length <= num){
+    return str;
+  } else if (num <= 2){
+    return str.slice(0, num)+"...";
+  } else{
+    return str.slice(0, num - 3)+"...";
+  }
 
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
