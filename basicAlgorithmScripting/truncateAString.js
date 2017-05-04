0e2e96d1af80be9cc127e5d5f89e0ca35cc0c2d1
Pedro Garboza <p.garboza@gmail.com>
2017-05-03 23:43:53 -0400

    Update truncateAString.js

--- basicAlgorithmScripting/truncateAString.js
+++ basicAlgorithmScripting/truncateAString.js
@@ -1,14 +1,20 @@
 function truncateString(str, num) {
   // Clear out that junk in your trunk
   
+  //if str length is less than equal return str
   if(str.length <= num){
     return str;
-  } else if (num <= 2){
+  } 
+  //if num less than of equal to two return str.slice(0, num)
+  else if (num <= 2){
     return str.slice(0, num)+"...";
-  } else{
+  } 
+  //else return str.slice(0, num - 3)
+  else {
     return str.slice(0, num - 3)+"...";
   }
 
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
