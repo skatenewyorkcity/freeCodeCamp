bae0598f01096e333ceff2740e76447a1d1cef92
Pedro Garboza <p.garboza@gmail.com>
2017-05-20 21:52:54 -0400

    Update mutations.js

--- basicAlgorithmScripting/mutations.js
+++ basicAlgorithmScripting/mutations.js
@@ -1,6 +1,18 @@
 
 function mutation(arr) {
-  return arr;
+  //The indexOf() method returns the position of the first occurrence of a specified value in a string.
+  //This method returns -1 if the value to search for never occurs.
+  
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  
+  for (var i = 0; i < test.length; i++){
+    if(target.indexOf(test[i]) < 0){
+      return false;
+    }
+  }
+ 
+  return true;
 }
 
 mutation(["hello", "hey"]);
