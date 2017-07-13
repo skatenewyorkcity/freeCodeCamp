0b2239caedff4ff092a3f34efbffa88fa825ab20
Pedro Garboza <p.garboza@gmail.com>
2017-07-13 07:50:28 -0400

    Update whereDoIBelong.js

--- basicAlgorithmScripting/whereDoIBelong.js
+++ basicAlgorithmScripting/whereDoIBelong.js
@@ -1,7 +1,11 @@
 function getIndexToIns(arr, num) {
-  // Find my place in this sorted array.
-  
-  return num;
+  arr.sort(function(a,b){
+    return a - b;
+  });
+  for (i = 0; i < arr.length; i++){
+    if(num <= arr[i]){
+      return i;  
+      } 
+  } return arr.length; 
 }
-
-getIndexToIns([40, 60], 50);
+getIndexToIns([10, 20, 30, 40, 50], 35); 
