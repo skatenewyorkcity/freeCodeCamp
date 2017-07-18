f942735d885d9c1d0cfdd4b6fde204102338ef49
Pedro Garboza <p.garboza@gmail.com>
2017-07-18 10:35:51 -0400

    Update ceasarsCipher.js

--- basicAlgorithmScripting/ceasarsCipher.js
+++ basicAlgorithmScripting/ceasarsCipher.js
@@ -1,6 +1,22 @@
 function rot13(str) { // LBH QVQ VG!
-  
-  return str;
+
+    var deciphered = [];
+    for (var i = 0; i < str.length; i++) {
+
+        if (str.charCodeAt(i) - 13 < 65) { if ((str.charCodeAt(i) >= 65) && (str.charCodeAt(i) <= 90)) {
+                deciphered.push(String.fromCharCode(90 + str.charCodeAt(i) - 65 - 12));
+            } else {
+                deciphered.push(String.fromCharCode(str.charCodeAt(i)));
+
+            }
+        } else {
+            deciphered.push(String.fromCharCode(str.charCodeAt(i) - 13));
+        }
+
+    }
+
+    return deciphered.join('');
+
 }
 
 // Change the inputs below to test
