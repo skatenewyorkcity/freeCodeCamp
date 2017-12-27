2c3fd3b12179f499099f4a64cfe0cd5aa5bc7529
Pedro Garboza <p.garboza@gmail.com>
2017-12-27 10:34:03 -0500

    Update sumAllNumbersInARange.js

--- intermediateAlgorithmScripting/sumAllNumbersInARange.js
+++ intermediateAlgorithmScripting/sumAllNumbersInARange.js
@@ -12,8 +12,16 @@
 // Array.prototype.reduce()
 
 function sumAll(arr) {
-  return 1;
+  var max = Math.max(arr[0], arr[1]);
+  var min = Math.min(arr[0], arr[1]);
   
+  var temp = 0;
+  
+  for(var i=min; i <= max; i++){
+    temp += i;
+  }
+  
+  return temp;
 }
 
 sumAll([1, 4]);
