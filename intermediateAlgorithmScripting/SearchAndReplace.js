c9b2e8acb36162d256e402401a8c9f8b4a9b5529
Pedro Garboza <p.garboza@gmail.com>
2018-01-09 10:54:02 -0500

    Update SearchAndReplace.js

--- intermediateAlgorithmScripting/SearchAndReplace.js
+++ intermediateAlgorithmScripting/SearchAndReplace.js
@@ -1,4 +1,12 @@
 function myReplace(str, before, after) {
+  
+  var index = str.indexOf(before);
+  
+  if(str[index] === str[index].toUpperCase()){
+    after = after.charAt(0).toUpperCase()+after.slice(1);
+  }
+  
+  str = str.replace(before, after);
   return str;
 }
 
