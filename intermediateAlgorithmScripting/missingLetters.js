c9c19d75459773583aa71000c031d8b91d6bc619
Pedro Garboza <p.garboza@gmail.com>
2018-01-17 09:37:01 -0500

    Update missingLetters.js

--- intermediateAlgorithmScripting/missingLetters.js
+++ intermediateAlgorithmScripting/missingLetters.js
@@ -1,5 +1,13 @@
 function fearNotLetter(str) {
-  return str;
+  
+  for(var i = 0; i < str.length; i++){
+    var code = str.charCodeAt(i);
+    
+    if(code !== str.charCodeAt(0) + i){
+      return String.fromCharCode(code - 1);
+    }
+  }
+  return undefined;
 }
 
 fearNotLetter("abce");
