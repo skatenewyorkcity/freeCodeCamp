785b7932286d434601e7d5f491731512f2f64a81
Pedro Garboza <p.garboza@gmail.com>
2018-01-23 12:46:39 -0500

    Update sortedUnion.js

--- intermediateAlgorithmScripting/sortedUnion.js
+++ intermediateAlgorithmScripting/sortedUnion.js
@@ -1,5 +1,19 @@
 function uniteUnique(arr) {
-  return arr;
+  var finalArray = [];
+  
+  for(var i = 0; i < arguments.length; i++){
+    var arrayArguments = arguments[i];
+    
+    for(var j = 0; j < arrayArguments.length; j++){
+      var indexValue = arrayArguments[j];
+      
+      if(finalArray.indexOf(indexValue) < 0){
+        finalArray.push(indexValue);
+      }
+    }
+  }
+  
+  return finalArray;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
