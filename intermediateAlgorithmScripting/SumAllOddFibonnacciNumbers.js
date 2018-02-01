62fd8c4bf970562c86a4300a512b065a1889341f
Pedro Garboza <p.garboza@gmail.com>
2018-02-01 09:08:04 -0500

    Update SumAllOddFibonnacciNumbers.js

--- intermediateAlgorithmScripting/SumAllOddFibonnacciNumbers.js
+++ intermediateAlgorithmScripting/SumAllOddFibonnacciNumbers.js
@@ -1,6 +1,24 @@
 
 function sumFibs(num) {
-  return num;
+  
+  var prevNumber = 0;
+  var currNumber = 1;
+  var result = 0;
+  
+  while (currNumber <= num){
+    
+    if(currNumber % 2 !== 0){
+     result += currNumber; 
+      
+    }
+    
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+    
+  }
+  
+  return result;
+  
 }
 
 sumFibs(4);
