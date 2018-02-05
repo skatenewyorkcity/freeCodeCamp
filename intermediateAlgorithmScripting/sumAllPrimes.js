bc30dccb2e0e60e0198c974eec66d0a1105ae00a
Pedro Garboza <p.garboza@gmail.com>
2018-02-05 09:16:40 -0500

    Update sumAllPrimes.js

--- intermediateAlgorithmScripting/sumAllPrimes.js
+++ intermediateAlgorithmScripting/sumAllPrimes.js
@@ -1,5 +1,33 @@
 function sumPrimes(num) {
-  return num;
+  var res = 0;
+  
+  
+  function getPrimes(max){
+    var sieve = [];
+    var i;
+    var j;
+    var primes = [];
+    
+    for (i = 2; i <= max; ++i){
+      if(!sieve[i]){
+        // i has not been marked -- it is prime
+        primes.push(i);
+        for(j = i << 1; j <= max; j += i){
+          sieve[j] = true;
+        }
+      }
+    }
+    
+    return primes;
+  }
+  
+  // add the primes
+  var primes = getPrimes(num);
+  for(var p = 0; p < primes.length; p++){
+    res += primes[p];
+  }
+  
+  return res;
 }
 
 sumPrimes(10);
