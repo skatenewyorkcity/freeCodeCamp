dd3e311122624e9dbde6e9367cb604831bbf2ee2
Pedro Garboza <p.garboza@gmail.com>
2018-02-07 11:29:17 -0500

    Update SmallestCommonMultiple.js

--- intermediateAlgorithmScripting/SmallestCommonMultiple.js
+++ intermediateAlgorithmScripting/SmallestCommonMultiple.js
@@ -1,5 +1,29 @@
 function smallestCommons(arr) {
-  return arr;
+  arr.sort(function(a, b){
+    return b - a;
+  });
+  
+  var newArr = [];
+  for(var i = arr[0]; i >= arr[1]; i--){
+    newArr.push(i);
+  }
+  
+  var quot = 0;
+  var loop = 1;
+  var n;
+  
+  do{
+    quot = newArr[0] * loop * newArr[1];
+    for(n = 2; n < newArr.length; n++){
+      if(quot % newArr[n] !== 0){
+        break;
+      }
+    }
+    
+    loop++;
+  }while(n !== newArr.length);
+  
+  return quot;
 }
 
 
