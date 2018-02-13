ac79197be6cf8b6152d04ee0d1e10df6e91c943e
Pedro Garboza <p.garboza@gmail.com>
2018-02-13 09:34:36 -0500

    Update DropElements.js

--- intermediateAlgorithmScripting/DropElements.js
+++ intermediateAlgorithmScripting/DropElements.js
@@ -1,6 +1,17 @@
 function dropElements(arr, func) {
   // Drop them elements.
+  
+  var times = arr.length;
+  
+  for(var i = 0; i < times; i++){
+    if(func(arr[0])){
+      break;
+    }else{
+      arr.shift();
+    }
+  }
+  
   return arr;
 }
 
-dropElements([1, 2, 3], function(n) {return n < 3; });
+dropElements([1, 2, 3, 4], function(n) {return n >= 3; });
