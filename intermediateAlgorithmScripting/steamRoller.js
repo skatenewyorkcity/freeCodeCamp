f78af9683cfc854ec5b105752bd70955d977543b
Pedro Garboza <p.garboza@gmail.com>
2018-02-15 09:03:26 -0500

    Update steamRoller.js

--- intermediateAlgorithmScripting/steamRoller.js
+++ intermediateAlgorithmScripting/steamRoller.js
@@ -1,6 +1,20 @@
 function steamrollArray(arr) {
   // I'm a steamroller, baby
-  return arr;
+  
+  var flattenedArray = [];
+  
+  var flatten = function(arg) {
+    if(!Array.isArray(arg)){
+      flattenedArray.push(arg);
+    }else{
+      for(var a in arg){
+        flatten(arg[a]);
+      }
+    }
+  };
+  
+  arr.forEach(flatten);
+  return flattenedArray;
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
