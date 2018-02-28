49e32644cd7efd9a967020fd418efd9d24f0c60d
Pedro Garboza <p.garboza@gmail.com>
2018-02-28 09:07:59 -0500

    Update EverythingBeTrue.js

--- intermediateAlgorithmScripting/EverythingBeTrue.js
+++ intermediateAlgorithmScripting/EverythingBeTrue.js
@@ -1,6 +1,13 @@
 function truthCheck(collection, pre) {
   // Is everyone being true?
-  return pre;
+  var counter = 0;
+  
+  for (var c in collection){
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])){
+      counter++;
+    }
+  }
+  return counter == collection.length;
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
