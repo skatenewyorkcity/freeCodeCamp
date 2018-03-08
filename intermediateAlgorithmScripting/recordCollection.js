afd9480cfbca0af13198a120040c0b7be59bd344
Pedro Garboza <p.garboza@gmail.com>
2018-03-08 09:26:58 -0500

    Update recordCollection.js

--- intermediateAlgorithmScripting/recordCollection.js
+++ intermediateAlgorithmScripting/recordCollection.js
@@ -1,3 +1,4 @@
+
 // Setup
 var collection = {
     "2548": {
@@ -30,10 +31,22 @@ var collectionCopy = JSON.parse(JSON.stringify(collection));
 // Only change code below this line
 function updateRecords(id, prop, value) {
   
-  
+  if(prop === "tracks" && value !== ""){
+    if(collection[id][prop]){
+      collection[id][prop].push(value);
+    }
+    else{
+      collection[id][prop]=[value];
+    }
+  }else if(value !== ""){
+    collection[id][prop] = value;
+  }else{
+    delete collection[id][prop];
+  }
   return collection;
 }
 
 // Alter values below to test your code
 updateRecords(5439, "artist", "ABBA");
+
 
