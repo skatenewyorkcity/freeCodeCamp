928316a2db990e0831e4ff5950c9c87c0c7abee3
Pedro Garboza <p.garboza@gmail.com>
2018-03-15 09:02:20 -0400

    Update SymmetricDifference.js

--- intermediateAlgorithmScripting/SymmetricDifference.js
+++ intermediateAlgorithmScripting/SymmetricDifference.js
@@ -1,6 +1,29 @@
+function sym(){
+  
+  var args = [];
+  
+  for (var i = 0; i < arguments.length; i++){
+    args.push(arguments[i]);
+  }
 
-function sym(args) {
-  return args;
+  function symDiff(arrayOne, arrayTwo){
+    var result = [];
+    
+    arrayOne.forEach(function(item){
+                     if(arrayTwo.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                     result.push(item);
+    }
+  });
+    
+    arrayTwo.forEach(function(item){
+                     if(arrayOne.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                     result.push(item);
+    }
+                     });
+  
+  return result;
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+  return args.reduce(symDiff);
+  
+}
