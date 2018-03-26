d2b2003312d09b3aa276989f692f165979b9b7fd
Pedro Garboza <p.garboza@gmail.com>
2018-03-26 09:24:25 -0400

    Update NoRepeatsPlease.js

--- intermediateAlgorithmScripting/NoRepeatsPlease.js
+++ intermediateAlgorithmScripting/NoRepeatsPlease.js
@@ -1,5 +1,40 @@
+
 function permAlone(str) {
-  return str;
+  
+  var regex = /(.)\1+/g;
+  
+  var arr = str.split('');
+  var permutations = [];
+  var tmp;
+  
+  if(str.match(regex) !== null && str.match(regex)[0] === str) return 0;
+  
+  function swap(index1, index2){
+    tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+    
+  }
+    
+  function generate(int){
+    if(int === 1){
+      permutations.push(arr.join(''));
+    }else{
+      for (var i = 0; i != int; ++i){
+        generate(int - 1);
+        swap(int % 2 ? 0 : i, int - 1);
+      }
+    }
+  }
+    
+    generate(arr.length);
+    
+    var filtered = permutations.filter(function(string){
+      return !string.match(regex);
+    });
+  
+    return filtered.length;
+
 }
 
 permAlone('aab');
