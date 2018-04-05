01ea51e06c297cdc73f4ed7498111a0f77eebfd9
Pedro Garboza <p.garboza@gmail.com>
2018-04-05 09:37:42 -0400

    Update Pairwise.js

--- intermediateAlgorithmScripting/Pairwise.js
+++ intermediateAlgorithmScripting/Pairwise.js
@@ -1,6 +1,19 @@
 
 function pairwise(arr, arg) {
-  return arg;
+  var sum = 0;
+  
+  var pairArr = arr.slice();
+  
+  for(i = 0; i < pairArr.length; i++){
+    for (j = i + 1; j < pairArr.length; j++){
+      if(pairArr[i] + pairArr[j] == arg){
+        sum += i + j;
+        pairArr[i] = pairArr[j] = NaN;
+      }
+    }
+  }
+  
+  return sum;
 }
 
 pairwise([1,4,2,3,0,5], 7);
