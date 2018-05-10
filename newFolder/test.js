390fb64f9d00045e0d4939e22576cfb7aa91acec
Pedro Garboza <p.garboza@gmail.com>
2018-05-10 12:26:07 -0400

    Update test.js

--- newFolder/test.js
+++ newFolder/test.js
@@ -7,19 +7,20 @@
 <p>
 <script>
 
-var getTrickCounter = 0;
+var getTrickCounter = -1;
 var retryTrickCounter = 0;
 var tricks = ["ollie", "nollie", "switchOllie", "fakieOllie"];
-
 var retryStat = [];
+
+
 
 function getTrick(t){
 	if(tricks[t] === undefined){
-      getTrickCounter = 0;
-      retryStat = [];
-      document.getElementById("tricks").innerHTML = " p";
+      getTrickCounter = -1;
+      document.getElementById("tricks").innerHTML = "EndOfTricks";
     }else{
       document.getElementById("tricks").innerHTML = tricks[t];
+      document.getElementById("trickAmmount").innerHTML = tricks.length;
     }
 }
 
@@ -43,15 +44,21 @@ function saveStatForTrick(r){
     document.getElementById("retryStat").innerHTML = retryStat;
 }
 
+function resetStat(){
+		if(retryStat.length === 5){
+		retryStat = [];
+        }
+}
+
 </script>
 </p>
 
 <p>
-Trick: <a id="tricks">  </a>
+Trick: <a id="tricks"> click "get trick" to begin</a>
 </p>
 
 <h3>
-<button type="button" onclick="getTrickOnClick(); getTrick(getTrickCounter); saveStatForTrick(retryTrickCounter); resetTrickCounter();">get trick</button>
+<button type="button" onclick="getTrickOnClick(); getTrick(getTrickCounter); saveStatForTrick(retryTrickCounter); resetTrickCounter(); resetStat(getTrickCounter);">get trick</button>
 <button type="button" onclick="retryTrickOnClick();">retry trick</button>
 </h3>
 
@@ -67,6 +74,9 @@ retryTrickOnClicked: <a id="retries">0</a>
 retryStat: <a id="retryStat">0</a>
 </p>
 
+<p>
+trickAmmount: <a id="trickAmmount">0</a>
+</p>
 
 </body>
 </html> 
